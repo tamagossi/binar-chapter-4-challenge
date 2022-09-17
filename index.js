c94629091b86c30a76d57f28c81b6b84bc45f39d
Raka Pratama <tama@sampingan.co.id>
2022-09-17 10:40:20 +0700

    feat(): add users to to do list

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let limit = 10;
 let todos = [];
 let page = 1;
+let keyword = '';
 
 window.addEventListener('load', function () {
 	getTodos();
@@ -21,11 +22,36 @@ const getTodos = async () => {
 	}
 };
 
-const renderTodoToDOM = (todos) => {
-	const slicedTodos = todos.slice(page - 1, page - 1 + limit);
-	console.log(slicedTodos);
+const getUserInformation = async (datas) => {
+	try {
+		const userIds = datas.map((todo) =>
+			fetch(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)
+		);
+		const result = await Promise.all(userIds);
+		const results = result.map((res) => res.json());
+		const users = await Promise.all(results);
 
-	const todoEl = slicedTodos.reduce((prev, current) => {
+		datas = datas.map((data, index) => ({ ...data, ...users[index] }));
+
+		return datas;
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+const renderTodoToDOM = async (todos) => {
+	let dataToRender = todos;
+
+	if (keyword && keyword !== '') {
+		dataToRender = todos.filter((todo) => {
+			if (todo.title.toLowerCase().includes(keyword.toLowerCase())) return todo;
+		});
+	}
+
+	dataToRender = dataToRender.slice((page - 1) * limit, page * limit);
+	dataToRender = await getUserInformation(dataToRender);
+
+	const todoEl = dataToRender.reduce((prev, current) => {
 		return (prev += `
 			<div class="todo-list-item drop-shadow-md bg-neutral-50 p-5">
 				<div class="grid grid-rows-1 justify-center text-center text-lg">
@@ -48,6 +74,12 @@ const renderTodoToDOM = (todos) => {
 							`
 					}
 				</div>
+
+				<div class="mt-3 text-sm">
+					Author: ${current.name}
+					Author email: ${current.email}
+					Author phone: ${current.phone}
+				</div>
 			</div>
 		`);
 	}, '');
@@ -57,8 +89,10 @@ const renderTodoToDOM = (todos) => {
 
 	if (page === 1) {
 		document.getElementById('previous-button').classList.add('disabled');
+		document.getElementById('previous-button').classList.add('cursor-none');
 	} else {
 		document.getElementById('previous-button').classList.remove('disabled');
+		document.getElementById('previous-button').classList.remove('cursor-none');
 	}
 };
 
@@ -78,4 +112,14 @@ const toTitleCase = (str = ' ') => {
 	return text.replace(/\w\S*/g, (txt) => {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 	});
+};
+
+const searchTodo = (e) => {
+	keyword = e.target.value;
+	renderTodoToDOM(todos);
+};
+
+const changePageLimit = (e) => {
+	limit = e.target.value;
+	renderTodoToDOM(todos);
 };
